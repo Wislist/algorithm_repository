c9dbfce482004af634cfeab49df2dce7f64e3365
Wislist <2929250563@qq.com>
2024-09-12 11:23:17 +0800

    提交gitignore

---
+++ acwing/快排.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int QuitSort(){
+
+
+}
+
+
+int main()
+{
+    int i,j;
+    int num;
+    cin >> num;
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+    
+} 
